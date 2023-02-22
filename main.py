891f2dcd5ee07b79c48664544d060c6144a22d6c
Cal122 <callumwork1122@gmail.com>
2023-02-22 05:19:01 +0000

    Added resizer function, completed image resizer

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from PIL import Image
 
-image = Image.open("random_logo.jpg")
 
-print(f"Current size : {image.size}")
+image = Image.open(r"(<directory to image>)")
+
+def resize_image(size1, size2):
+
+    print(f"Current size : {image.size}")
+
+    resized_image = image.resize((size1, size2))
+
+    resized_image.save(f"Resized_input{size1}x{size2}.jpg")
+
+size1 = int(input("Desired width: "))
+size2 = int(input("Desired Height: "))
+
+resize_image(size1, size2)
